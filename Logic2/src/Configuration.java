3cccd2d9a31af98e50dbe10c7f13964fe11d7ae4
stoyanovd <stoyanovd@mail.ru>
2014-03-11 19:06:11 +0400

    Delete old versions

--- Logic2/src/Configuration.java
+++
@@ -1,8 +0,0 @@
-public class Configuration {
-
-    public static final boolean DEBUG_MODE = false;
-
-    public static final String FILE_FOR_DEDUCTION = "a0.in";
-    public static final String FILE_FOR_DEDUCTION_RESULT = "a.out";
-    public static final String FILE_FOR_CHECK_OF_RESULT = "ac.out";
-}
